adaf5a14de95d45a6dfd1666f70b044f79f2d1ff
xi <1805074477@qq.com>
2024-07-09 14:56:22 +0800

    day03:实现菜品分页查询

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,6 +1,8 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
@@ -32,5 +34,17 @@ public class DishController {
         log.info("新增菜品：{}", dishDTO);
         dishService.save(dishDTO);//后绪步骤开发
         return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     */
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public  Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+
+        log.info("菜品分页查询:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);//后绪步骤定义
+        return Result.success(pageResult);
     }
 }
